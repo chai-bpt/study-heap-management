82066207cf436afdfb57ea9a3106c2e7c2e29a7f
chai-bpt <chai.bpt@gmail.com>
2021-07-04 20:07:07 +0530

    current top of heap memory captured using sbrk(0)

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
-
+#include<unistd.h>
 int main(void)
 {
+	void* current_brk = NULL;
+	
 	printf("\n\t%s\t%s\t%s\t%s\t%d\tSTART",__DATE__,__TIME__,__FILE__,__func__,__LINE__);
+	
+	current_brk = sbrk(0);
+
+	printf("\n\tcurrent_brk = %p",current_brk);
+
 	printf("\n\t%s\t%s\t%s\t%s\t%d\tEND\n",__DATE__,__TIME__,__FILE__,__func__,__LINE__);
 }
